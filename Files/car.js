47eba623dd391e230d3e40fef6b5f3b9cf3596bc
Duan, Daniel Lee <dduan6@gatech.edu>
2022-06-03 04:32:30 -0400

    implement neural network

--- Files/car.js
+++ Files/car.js
@@ -10,9 +10,11 @@ class Car {
         this.friction = 0.04;
         this.angle = 0;
         this.damaged = false;
+        this.useBrain = controlType == "AI";
 
         if (controlType != "DUMMY") {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
         }
 
         this.controls = new Controls(controlType);
@@ -24,8 +26,18 @@ class Car {
             this.polygon = this.#createPolygon();
             this.damaged = this.#assessDamage(roadBorders, traffic);
         }
+
         if (this.sensor) {
             this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map(s => s == null ? 0 : 1 - s.offset);
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+            // console.log(outputs);
+            if (this.useBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
@@ -94,7 +106,7 @@ class Car {
         this.y -= Math.cos(this.angle) * this.vel;
     }
 
-    draw(ctx, color) {
+    draw(ctx, color, drawSensor = false) {
         if (this.damaged) {
             ctx.fillStyle = "red";
         } else { 
@@ -107,7 +119,7 @@ class Car {
         }
         ctx.fill();
         
-        if (this.sensor) {
+        if (this.sensor && drawSensor) {
             this.sensor.draw(ctx);
         }
     }
